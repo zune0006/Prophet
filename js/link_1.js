996c01ded3ba07cfad20d1c9b5bd7c1ad16138bb
zune0006 <126517305+zune0006@users.noreply.github.com>
2025-06-29 23:52:28 +0200

    Update link_1.js

--- js/link_1.js
+++ js/link_1.js
@@ -1,5 +1,55 @@
 
 var iztro = require("iztro");
 
+var { astro } = require("iztro");
+
+// 通过阳历获取星盘信息
+var astrolabe = astro.bySolar("2000-8-16", 2, "女");
+
+// 通过农历获取星盘信息
+var astrolabe = astro.byLunar("2000-7-17", 2, "女", false);
+
+astrolabe.horoscope(new Date());
+
+import { HeavenlyStemName } from "iztro/lib/i18n";
+import { Palace } from "iztro/lib/data/types";
+
+import { setLanguage } from "iztro/lib/i18n";
+
+setLanguage("ko-KR");
+
+const horoscopeStar = getHoroscopeStar("甲", "zi", "decadal");
+
+export type Language = `zh-CN` | `zh-TW` | `en-US` | `ko-KR` | `ja-JP`;
+
+export type Scope = "origin" | "decadal" | "yearly";
+
+export type StarType =
+  | "major"
+  | "soft"
+  | "tough"
+  | "adjective"
+  | "flower"
+  | "helper"
+  | "lucun"
+  | "tianma";
+
+
+export type Star = {
+  name: StarName;
+  type: StarType;
+  scope: Scope;
+  brightness?: Brightness;
+  mutagen?: Mutagen;
+};
+
+
+export type SoulAndBody = {
+  soulIndex: number;
+  bodyIndex: number;
+  heavenlyStemOfSoul: HeavenlyStemName;
+  earthlyBranchOfSoul: EarthlyBranchName;
+};
+
 
 
